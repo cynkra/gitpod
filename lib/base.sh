30d77f25630fc8382810cba4edfe9014d6c3c098
Antonov Misha <antonov5551998@gmail.com>
2023-02-28 14:33:33 +0000

    add base script

---
+++ lib/base.sh
@@ -0,0 +1,31 @@
+# Set up rig
+curl -Ls https://github.com/r-lib/rig/releases/download/latest/rig-linux-latest.tar.gz | sudo tar xz -C /usr/local
+# Set up R
+rig install
+rig system add-pak
+rig system make-links
+rig system setup-user-lib
+
+# Deps
+sudo apt update
+sudo apt install -y ccache cmake r-base libharfbuzz-dev libfribidi-dev
+
+## Set up ccache
+ln -s /usr/lib/ccache/* ~/bin/
+
+## Work around glitch with non-systemd systems
+ln -s $(which true) ~/bin/timedatectl
+
+## Define PATH
+# FIXME: Why is this necessary? This doesn't work in Radian.
+echo 'export PATH='${HOME}'/bin:${PATH}' >> ~/.bashrc
+
+# Set up Makevars
+mkdir -p ~/.R
+echo -e "MAKEFLAGS = -j8\nCXXFLAGS = -O0 -g" > ~/.R/Makevars
+
+# Install R packages
+echo 'options(repos = "https://packagemanager.rstudio.com/all/__linux__/'$(cat /etc/lsb-release | sed  -n '/DISTRIB_CODENAME=/ {s///;p}')'/latest")' >> ~/.Rprofile
+
+# Install radian
+sudo pip install radian
