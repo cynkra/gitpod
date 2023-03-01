d01a556ae8f6578e3c823a0fbc215ae2eb27b455
Kirill Müller <krlmlr+github@mailbox.org>
2023-03-01 04:59:51 +0000

    Prepare mariadb for testing

--- lib/mariadb.sh
+++ lib/mariadb.sh
@@ -9,3 +9,6 @@ sudo service mysql start
 
 ## Configure test database
 sudo mysql -e "CREATE DATABASE IF NOT EXISTS test; ALTER DATABASE test CHARACTER SET 'utf8'; FLUSH PRIVILEGES;"
+sudo mysql -e "CREATE USER IF NOT EXISTS '$USER'@'localhost' IDENTIFIED BY ''; GRANT ALL PRIVILEGES ON *.* TO '$USER'@'localhost'; FLUSH PRIVILEGES;"
+sudo mysql -D mysql < $(dirname $0)/mariadb-tz.sql
+sudo mysql -e "SET GLOBAL time_zone = 'Europe/Zurich';"
