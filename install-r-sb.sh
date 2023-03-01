e7bd32e9e885f29e9578f849b03521dc142c9751
Kirill Müller <krlmlr+github@mailbox.org>
2023-03-01 04:14:12 +0000

    Install from tarball

--- install-r-sb.sh
+++ install-r-sb.sh
@@ -1,24 +1,19 @@
 # Error handling
 set -eux
 
+cd $(dirname $0)
+
 # Run base script
 sh ./lib/base.sh
 
-# clone r-svn repository
-if ! [ -d r-svn ]; then
-  git clone https://github.com/r-devel/r-svn
-fi
+# Download R 4.2.2
+curl -L -o R-sb.tar.gz $(Rscript -e 'writeLines(rversions::r_release_tarball()$URL)')
+mkdir -p R-sb
+cd R-sb
+tar xf ../R-sb.tar.gz --strip-components=1
+rm ../R-sb.tar.gz
 
-cd r-svn
-
-sudo apt-get update -y
-sudo apt-get upgrade -y
 sudo apt-get install -y clang gcc wget locales git rsync gfortran xvfb autoconf pkg-config texinfo texlive-latex-extra texlive-fonts-recommended tk8.6-dev libcurl4-openssl-dev libblas-dev libbz2-dev libicu-dev libjpeg-dev liblapack-dev liblzma-dev libncurses5-dev libpcre2-dev libpng-dev libreadline-dev libxt-dev
-sudo localedef -i en_US -f UTF-8 en_US.UTF-8
-
-sed -i.bak 's|$(GIT) svn info|./.github/scripts/svn-info.sh|' Makefile.in
-#./.github/scripts/wget-recommended.sh
-./.github/scripts/svn-info.sh
 
 ./configure  --without-recommended-packages --enable-R-shlib --with-blas --with-lapack --disable-java --enable-strict-barrier
 
